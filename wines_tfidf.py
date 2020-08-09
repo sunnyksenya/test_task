322c4a8125ba8b133931d9e41f8edc308d451eb9
sunnyksenya <oksanavdovenko2014@gmail.com>
2020-08-09 22:06:15 +0300

    Add files via upload

--- wines_tfidf.py
+++ wines_tfidf.py
@@ -32,41 +32,49 @@ class_list = [0,1,2,3,4]#['Low', 'OK', 'Good', 'Very Good', 'Excellent']
 # create a new column and use np.select to assign values to it using our lists as arguments
 data['class'] = np.select(conditions, class_list)
 
-data = data[['description','class']]
-data = data.dropna()
+data = data[['description','winery', 'country','class']] # selecting columns
+data = data.dropna() # deleting na rows
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(data['description'], data['class'])
+data["united"] = data["country"] + " " + data['winery'] + " " + data["description"]
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(data['united'],
+                                                                    data['class'],
+                                                                    test_size=0.33,
+                                                                    random_state=42)
 
 
 encoder = preprocessing.LabelEncoder()
 y_train = encoder.fit_transform(y_train)
 y_test = encoder.fit_transform(y_test)
 
-# word level tf-idf
-tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
-tfidf_vect.fit(data['description'])
+# create a  vectorizer object 
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+tfidf_vect.fit(data['united'])
 
+# transform the training and validation data using count vectorizer object
 X_train =  tfidf_vect.transform(X_train)
 X_test =  tfidf_vect.transform(X_test)
 
 
 
 rf = load('tfidf/RandomForestClassifier_tfidf.joblib')
-nb = load('tfidf/MultinomialNB_tfidf.joblib')
+nb = load('tfidf/MultinomialNB_count.joblib')
 lg = load('tfidf/LogisticRegression_tfidf.joblib')
-#mlp = load('MLPClassifier_tfidf.joblib')
+mlp = load('tfidf/MLPClassifier_tfidf.joblib')
+dt = load('tfidf/DecisionTreeClassifier_tfidf.joblib')
 xgb = load('tfidf/XGBClassifier_tfidf.joblib')
 
 
 rf_pred = rf.predict(X_test)
 nb_pred = nb.predict(X_test)
 lg_pred = lg.predict(X_test)
-#mlp_pred = mlp.predict(X_test)
+mlp_pred = mlp.predict(X_test)
+dt_pred = dt.predict(X_test)
 xgb_pred = xgb.predict(X_test)
-
 
 print( "rf",metrics.accuracy_score(rf_pred, y_test),classification_report(y_test,rf_pred), sep = '\n')
 print( "nb",metrics.accuracy_score(nb_pred, y_test),classification_report(y_test,nb_pred),sep = '\n')
 print( "lg",metrics.accuracy_score(lg_pred, y_test),classification_report(y_test,lg_pred),sep = '\n')
-#print( "mlp",metrics.accuracy_score(mlp_pred, y_test),classification_report(y_test,mlp_pred),sep = '\n')
+print( "dt",metrics.accuracy_score(dt_pred, y_test),classification_report(y_test,dt_pred),sep = '\n')
+print( "mlp",metrics.accuracy_score(mlp_pred, y_test),classification_report(y_test,mlp_pred),sep = '\n')
 print( "xgb",metrics.accuracy_score(xgb_pred, y_test),classification_report(y_test,xgb_pred),sep = '\n')
